f20fbcf96a822d5f55236eab8083909ef978e30d
Jan Sorgalla <jan@sorgalla.com>
2012-10-02 12:53:18 +0200

    Refactoring

--- Readme.md
+++ Readme.md
@@ -40,22 +40,21 @@ You can pass a set of these options to the initialize function to set a custom b
         <th>Description</th>
     </tr>
     <tr>
-        <td><strong>apiParameter</strong> (object)</td>
+        <td><strong>ajax</strong> (object)</td>
         <td><pre><code>{
-    screen_name     : 'twitter',
-    since_id        : null,
-    max_id          : null,
-    page            : 1,
-    trim_user       : true,
-    include_rts     : false,
-    exclude_replies : true
+    url: 'http://api.twitter.com/1/statuses/user_timeline.json',
+    data: {
+        screen_name     : 'twitter',
+        since_id        : null,
+        max_id          : null,
+        page            : 1,
+        trim_user       : true,
+        include_rts     : false,
+        exclude_replies : true
+    },
+    dataType: 'jsonp'
 }</code></pre></td>
-        <td>These parameters will be used to call the twitter API. You can find a complete list of valid parameters [here](https://dev.twitter.com/docs/api/1/get/statuses/user_timeline)</td>
-    </tr>
-    <tr>
-        <td><strong>apiUrl</strong> (string)</td>
-        <td>http://api.twitter.com/1/ statuses/user_timeline.json</td>
-        <td>URL of the API that will be called with a JSONP-Call. Remind, that if you change the URL the expected parameters may vary!</td>
+        <td>Options passed to <code>$.ajax()</code>. Fot the <code>data</code> part, you can find a complete list of valid parameters [here](https://dev.twitter.com/docs/api/1/get/statuses/user_timeline)</td>
     </tr>
     <tr>
         <td><strong>count</strong> (integer)</td>
@@ -65,31 +64,23 @@ You can pass a set of these options to the initialize function to set a custom b
     <tr>
         <td><strong>refresh</strong> (boolean|integer)</td>
         <td>false</td>
-        <td>If set to a numeric value, the timeline will be refreshed every x seconds. New tweets will be prepended to the list, and old tweets will be deleted to maintain the maximum number specified with the option count</td>
-    </tr>
-    <tr>
-        <td><strong>el</strong> (string)</td>
-        <td>p</td>
-        <td>Wrap this element around each tweet</td>
+        <td>If set to a numeric value, the timeline will be refreshed every x milliseconds. New tweets will be prepended to the list, and old tweets will be deleted to maintain the maximum number specified with the option count</td>
     </tr>
     <tr>
         <td><strong>tweetTemplate</strong> (object)</td>
-        <td>function(item) { return item.text.parseTweet(); }</td>
-        <td>Function to render each tweet. The tweet data is passed as an argument, and the plugin is accessible via the this variable.</td>
-    </tr>
-    <tr>
-        <td><strong>loader</strong> (boolean|string)</td>
-        <td>false</td>
-        <td>Class name of a loader placeholder that is inside the tweet container. Every element with this class will be removed with the animateRemove method on the first fetch call</td>
+        <td><pre><code>function(item) {
+    return '<p>' + TwitterTimeline.parseTweet(item.text) + '</p>';
+}</code></pre></td>
+        <td>Function to render each tweet. The tweet data is passed as an argument, and the plugin is accessible via the <code>this</code> variable.</td>
     </tr>
     <tr>
         <td><strong>animateAdd</strong> (object)</td>
-        <td>function(el) { return el; }</td>
+        <td><pre><code>function(el) { return el; }</code></pre></td>
         <td>Animate method to add elements. This method has to return the new element. If not, the element will not be added to the DOM.</td>
     </tr>
     <tr>
         <td><strong>animateRemove</strong> (object)</td>
-        <td>function(el) { el.remove(); }</td>
+        <td><pre><code>function(el) { el.remove(); }</code></pre></td>
         <td>Animate method to remove elements. This method has to remove the element from the DOM!</td>
     </tr>
 </table>
@@ -106,25 +97,20 @@ The tweetTimeline plugin provieds the following public methods:
         <th>Description</th>
     </tr>
     <tr>
-        <td><strong>getTweets(options)</strong></td>
-        <td>(optional) object options</td>
-        <td>Fetch new Tweets from the API and add them to the containert. The optional options parameter extends/overwrites the apiParameters</td>
+        <td><strong>fetch(options)</strong></td>
+        <td>(optional) ajax options</td>
+        <td>Fetch new Tweets from the API and add them to the containert. The optional ajax parameter extends the plugins ajax options.</td>
     </tr>
     <tr>
-        <td><strong>updateTweets(items)</strong></td>
+        <td><strong>update(items)</strong></td>
         <td>array items</td>
-        <td>Manually add tweets to the container</td>
-    </tr>
-    <tr>
-        <td><strong>parseTweet(text)</strong></td>
-        <td>string text</td>
-        <td>Parse a string (preferably the tweet text) and change all urls to links, usernames to profile links and link hashtags to the twitter search for that hashtag</td>
+        <td>Manually add tweets to the container.</td>
     </tr>
 </table>
 
 If you want to manually refresh the tweet list for example, you can use the following line to fetch new tweets:
 
-    $('#tweetContainer').data('twitterTimeline').getTweets();
+    $('#tweetContainer').twitterTimeline('fetch');
 
 
 Example settings
@@ -135,16 +121,17 @@ The following example is used for the tweet box you can find on our [website](ht
     <script type="text/javascript">
         $(function() {
             $('#tweetContainer').twitterTimeline({
-                apiParameter: {
-                    screen_name: 'dotsunited',
-                    count: 1
+                ajax: {
+                    data: {
+                        screen_name: 'dotsunited',
+                        count: 1
+                    }
                 },
                 count: 1,
-                el: 'div',
                 tweetTemplate: function(item) {
                     var dateRegEx = /^(Mon|Tue|Wed|Thu|Fri|Sat|Sun) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) ([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2}) \+([0-9]{4}) ([0-9]{4})$/;
                     var date = dateRegEx.exec(item.created_at);
-                    var tweet = '<div class="date">' + date[3] + ' '  + date[2] + ' '  + date[8] + '</div><p>' + item.text.parseTweet() + '</p>';
+                    var tweet = '<p><div class="date">' + date[3] + ' '  + date[2] + ' '  + date[8] + '</div><p>' + $.twitterTimeline.parseTweet() + '</p></p>';
                     return tweet;
                 },
                 animateAdd: function(el) {
--- jquery.twitterTimeline.js
+++ jquery.twitterTimeline.js
@@ -5,98 +5,123 @@
  *  License: MIT (http://www.opensource.org/licenses/mit-license.php)
  */
 
-;(function($) {
+(function($) {
 
-    // Create the defaults once
-    var pluginName = 'twitterTimeline',
-        defaults = {
-            apiParameter    : {
-                screen_name     : 'twitter',
-                since_id        : null,
-                max_id          : null,
-                page            : 1,
-                trim_user       : true,
-                include_rts     : false,
-                exclude_replies : true
-            },
-            apiUrl          : 'http://api.twitter.com/1/statuses/user_timeline.json',
+    var TwitterTimeline = $.twitterTimeline = function(element, options) {
+        this.element  = $(element);
+        this.options  = $.extend({}, this.options, options || {});
 
-            count           : 5,
-
-            refresh         : false,
-            el              : 'p',
-            tweetTemplate   : function(item) {
-                return this.parseTweet(item.text);
-            },
-            loader          : false,
-            animateAdd      : function(el) {
-                return el;
-            },
-            animateRemove   : function(el) {
-                el.remove();
-            }
-        };
-
-    // The actual plugin constructor
-    function Plugin(element, options) {
-        this.element  = element;
-        this.options  = $.extend( {}, defaults, options);
-        this.interval = null;
-
-        this.useLocalStorage = typeof localStorage !== 'undefined' && typeof JSON !== 'undefined';
-        this.localStorageKey = pluginName + '_' + this.options.apiParameter.screen_name;
-
-        this.init();
-    }
-
-    Plugin.prototype.updateTweets = function(data) {
-        var self = this;
-
-        if (this.options.loader) {
-            this.options.animateRemove($(this.options.loader, this.element));
-        }
-
-        // update localStorage
-        if (this.useLocalStorage === true && data.length > 0) {
-            var cache = localStorage.getItem(self.localStorageKey);
-            cache = cache !== null ? JSON.parse(cache) : [];
-            cache = data.concat(cache).splice(0, this.options.count);
-            cache = JSON.stringify(cache);
-            try {
-                localStorage.removeItem(self.localStorageKey); // http://stackoverflow.com/questions/2603682/is-anyone-else-receiving-a-quota-exceeded-err-on-their-ipad-when-accessing-local
-                localStorage.setItem(self.localStorageKey, cache);
-            } catch (e) {
-                // local storage should not be used because of security reasons like private browsing
-                // disable for further updates
-                self.useLocalStorage = false;
-            }
-        }
-
-        //add new tweets
-        $(data.reverse()).each(function(idx, item) {
-
-            //set since_id to current tweet for further updates
-            self.options.apiParameter.since_id = item.id_str;
-
-            //get tweet html from template and prepend to list
-            var tweet = self.options.tweetTemplate.call(self, item);
-            if (self.options.el) {
-                tweet = '<' + self.options.el + '>' + tweet + '</' + self.options.el + '>';
-            }
-            $(self.element).prepend(self.options.animateAdd($(tweet), idx));
-
-            //remove last tweet if the number of elements is bigger than the defined count
-            var tweets = $(self.element).children(self.options.el);
-            if (tweets.length > self.options.count) {
-                self.options.animateRemove($(tweets[self.options.count]), idx);
-            }
-
-        });
-
+        this._init();
     };
 
-    Plugin.prototype.parseTweet = function(text) {
+    $.extend(TwitterTimeline.prototype, {
+        options: {
+            ajax: {
+                url: 'http://api.twitter.com/1/statuses/user_timeline.json',
+                data: {
+                    screen_name     : 'twitter',
+                    since_id        : null,
+                    max_id          : null,
+                    page            : 1,
+                    trim_user       : true,
+                    include_rts     : false,
+                    exclude_replies : true
+                },
+                dataType: 'jsonp'
+            },
+            count: 5,
 
+            refresh: false,
+            tweetTemplate : function(item) {
+                return '<p>' + TwitterTimeline.parseTweet(item.text) + '</p>';
+            },
+            animateAdd: function(el) {
+                return el;
+            },
+            animateRemove: function(el) {
+                el.remove();
+            }
+        },
+        _refreshTimeout: null,
+        _init: function() {
+            this._useLocalStorage = typeof localStorage !== 'undefined' && typeof JSON !== 'undefined';
+            this._localStorageKey = 'twitterTimeline_' + this.options.apiParameter.screen_name;
+
+            //read localStorage and write tweets if there are cached ones
+            if (this._useLocalStorage === true) {
+                var cache = localStorage.getItem(this._localStorageKey);
+                if (cache !== null) {
+                    cache = JSON.parse(cache);
+                    if (cache.length > 0 ) {
+                        this.update(cache);
+                    }
+                }
+            }
+
+            //get new tweets
+            this.fetch();
+        },
+        update: function(data) {
+            var self = this;
+
+            if (this._refreshTimeout) {
+                clearTimeout(this._refreshTimeout);
+            }
+
+            // update localStorage
+            if (this._useLocalStorage === true && data.length > 0) {
+                var cache = localStorage.getItem(this._localStorageKey);
+                cache = cache !== null ? JSON.parse(cache) : [];
+                cache = data.concat(cache).splice(0, this.options.count);
+                cache = JSON.stringify(cache);
+                try {
+                    localStorage.removeItem(this._localStorageKey); // http://stackoverflow.com/questions/2603682/is-anyone-else-receiving-a-quota-exceeded-err-on-their-ipad-when-accessing-local
+                    localStorage.setItem(this._localStorageKey, cache);
+                } catch (e) {
+                    // local storage should not be used because of security reasons like private browsing
+                    // disable for further updates
+                    this._useLocalStorage = false;
+                }
+            }
+
+            //add new tweets
+            $.each(data.reverse(), function(idx, item) {
+                //set since_id to current tweet for further updates
+                self.options.apiParameter.since_id = item.id_str;
+
+                //get tweet html from template and prepend to list
+                var tweet = self.options.tweetTemplate.call(self, item);
+                self.element.prepend(self.options.animateAdd($(tweet), idx));
+
+                //remove last tweet if the number of elements is bigger than the defined count
+                var tweets = self.element.children(self.options.el);
+                if (tweets.length > self.options.count) {
+                    self.options.animateRemove($(tweets[self.options.count]), idx);
+                }
+            });
+
+            if (typeof this.options.refresh === 'number') {
+                this._refreshTimeout = setTimeout($.proxy(this.fetch, this), this.options.refresh);
+            }
+        },
+        fetch: function(options) {
+            var ajax = $.extend({}, this.options.ajax, options || {});
+            
+            var self = this,
+                success = ajax.success;
+                
+            ajax.success = function(data) {
+                self.update(data);
+                if ($.isFunction(success)) {
+                    success.apply(this, arguments);
+                }
+            };
+
+            $.ajax(ajax);
+        }
+    });
+
+    TwitterTimeline.parseTweet = function(text) {
         text = text.replace(/(\b(https?|ftp|file):\/\/[\-A-Z0-9+&@#\/%?=~_|!:,.;]*[\-A-Z0-9+&@#\/%=~_|])/ig, function(url) {
             return '<a href="' + url + '" target="_blank">' + url + '</a>';
         });
@@ -112,46 +137,21 @@
         return text;
     };
 
-    Plugin.prototype.getTweets = function(options) {
+    $.fn.twitterTimeline = function(options) {
+        if (typeof options === 'string') {
+            var instance = $(this).data('twitterTimeline');
+            return instance[options].apply(instance, Array.prototype.slice.call(arguments, 1));
+        } else {
+            return this.each(function() {
+                var instance = $(this).data('twitterTimeline');
 
-        var parameter = options || this.options.apiParameter;
-
-        $.ajax({
-            url         : this.options.apiUrl,
-            crossDomain : true,
-            data        : parameter,
-            dataType    : 'jsonp',
-            success     : $.proxy(this.updateTweets, this)
-        });
-    };
-
-    Plugin.prototype.init = function() {
-
-        if (typeof this.options.refresh === 'number') {
-            this.interval = setInterval($.proxy(this.getTweets, this), this.options.refresh * 1000);
-        }
-
-        //read localStorage and write tweets if there are cached ones
-        if (this.useLocalStorage === true) {
-            var cache = localStorage.getItem(this.localStorageKey);
-            if (cache !== null) {
-                cache = JSON.parse(cache);
-                if (cache.length > 0 ) {
-                    this.updateTweets(cache);
+                if (instance) {
+                    $.extend(instance.options, options || {});
+                } else {
+                    new TwitterTimeline(this, options);
                 }
-            }
+            });
         }
-
-        //get new tweets
-        this.getTweets();
-    };
-
-    $.fn[pluginName] = function(options) {
-        return this.each(function() {
-            if (!$.data(this, pluginName)) {
-                $.data(this, pluginName, new Plugin(this, options));
-            }
-        });
     };
 
 })(jQuery);
